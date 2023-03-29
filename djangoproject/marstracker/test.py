b1e4d063e43edc56283cb1020802aa886b9b8eda
Arseniy Nikonov <nikon.bmstu@gmail.com>
2023-03-29 12:50:18 +0200

    Simple test for player CRUD

---
+++ djangoproject/marstracker/test.py
@@ -0,0 +1,32 @@
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+from .models import Player
+
+class PlayerTest(TestCase):
+
+    def setUp(self):
+        self.player = Player.objects.create(first_name = "test", last_name = "testov")
+       
+
+    def test_create_player(self):
+        #player = Player.objects.create(first_name = "test", last_name = "testov")
+        self.assertEqual(Player.objects.count(),1)
+        self.assertEqual(self.player.first_name,"test")
+        self.assertEqual(self.player.last_name,"testov")
+    
+    def test_get_player(self):
+        player = Player.objects.get(id=self.player.id)
+        self.assertEqual(player.first_name,"test")
+        self.assertEqual(player.last_name,"testov")
+
+    def test_update_player(self):
+        self.player.first_name = "update_test"
+        self.player.save()
+        player = Player.objects.get(id=self.player.id)
+        self.assertEqual(player.first_name,"update_test")
+
+    def test_delete_player(self):
+        self.player.delete()
+        self.assertEqual(Player.objects.count(), 0)
